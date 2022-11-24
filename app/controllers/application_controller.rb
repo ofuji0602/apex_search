6ddc6cb04e9a47fab10646150f9fa36cde86377d
ofuji0602 <hitujimofumofu0206@gmail.com>
2022-11-24 14:36:56 +0900

    Modify:コントローラーのスペルミスを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameer_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
 end

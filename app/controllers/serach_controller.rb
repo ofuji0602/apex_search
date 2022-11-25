e10cd0d83e86e77ed176d10c27bee36707c0865e
ofuji0602 <hitujimofumofu0206@gmail.com>
2022-11-26 07:06:09 +0900

    Add:serach用のコントローラー作成

---
+++ app/controllers/serach_controller.rb
@@ -0,0 +1,2 @@
+class SerachController < ApplicationController
+end
---
+++ test/controllers/serach_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SerachControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

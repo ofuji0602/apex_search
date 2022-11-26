76ad4f53304665714d4945e5ec7e8cd4660bcfc6
ofuji0602 <hitujimofumofu0206@gmail.com>
2022-11-26 21:12:24 +0900

    Add:searchesコントローラー作成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,27 @@
+require 'google/apis/youtube_v3'
+require 'active_support/all'
+
 class SearchesController < ApplicationController
+  GOOGLE_API_KEY = Rails.application.credentials.google[:api_key]
+
   def index
+    @youtube_data = find_videos('明日香ちゃんねる')
+  end
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = GOOGLE_API_KEY
+
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: 'video',
+      max_results: 3,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601
+    }
+    service.list_searches(:snippet, opt)
   end
 end

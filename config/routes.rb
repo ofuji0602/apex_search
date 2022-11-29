fc833b57609fb1831ac79c53213a4e7330e0a544
ofuji0602 <hitujimofumofu0206@gmail.com>
2022-11-30 03:16:43 +0900

    Add:Topコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'tops/index'
   root 'pages#index'
   get 'pages/show'
   devise_for :users
+  get 'rank/index', to: 'rank#index'
   get 'searches/index', to: 'searches#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
